3124c7501f9553d73deead0fc1d76bcf63a40af7
kckey <57681807+kckey@users.noreply.github.com>
2021-03-17 23:39:59 -0400

    Update ADXL345.py
    added forgotten parenthesis for time.time

--- Drivers/ADXL345.py
+++ Drivers/ADXL345.py
@@ -14,11 +14,11 @@ accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 #c.writerow(["X-Value","Y-Value","Z-Value","Time"]) ~writes first row of csv file
 
-#start_time = time.time ~function to start timing
+#start_time = time.time() ~function to start timing
 
 while True:
     print("%f %f %f"%accelerometer.acceleration)
-#   current_time = time.time ~assing in time measurement
+#   current_time = time.time() ~assing in time measurement
 #   elapsed_time = current_time - start_time
 #   c.writerow([accelerometer.X, accelerometer.Y, accelerometer.Z,elapsed_time]) ~calls X,Y,& Z values from accelerometer and plots them into csv
     time.sleep(0.5) # i would change time to (.001) for better data
