8df7a616d845072d94ba03f2fbbae76d1971d5c8
gilgarwld <78825647+gilgarwld@users.noreply.github.com>
2021-04-15 16:13:42 -0400

    Update pwm1.py

--- Drivers/pwm1.py
+++ Drivers/pwm1.py
@@ -1,21 +1,32 @@
-import RPi.GPIO as GPIO     # Importing RPi library to use the GPIO pins
-from time import sleep  # Importing sleep from time library
+import RPi.GPIO as GPIO     
+from time import sleep  
 led_pin = 21            # Initializing the GPIO pin 21 for vibration sensor
 GPIO.setmode(GPIO.BCM)          # We are using the BCM pin numbering
 GPIO.setup(led_pin, GPIO.OUT)   # setting pin 21 as output pin
-pwm = GPIO.PWM(led_pin, 100)    # PWM object is created
+pwm = GPIO.PWM(led_pin, 100)    # PWM object is created at 100Hz
 pwm.start(0)                    # Started PWM at 0% duty cycle
 try:
-    test_text = input ("Enter a number: ")
+    test_text = input ("Enter a number between 0 and 3: ")
     test_text = int(test_text)
     while 1:                    # Loop will run forever
-    
-            pwm.ChangeDutyCycle(test_text) # Change duty cycle
-            sleep(0.01)         # Delay of 10mS
-            
-
+    if test_text =0:
+         pwm.ChangeDutyCycle(0) # Change duty cycle
+            sleep(0.01)  
+    elif test_text =1 :
+            pwm.ChangeDutyCycle(25) # Change duty cycle
+            sleep(0.01)         
+    elif test_text =2:
+       pwm.ChangeDutyCycle(100) # Change duty cycle
+            sleep(0.01)
+    elif test_text =3: 
+         pwm.ChangeDutyCycle(100) # Change duty cycle
+            sleep(0.01)
+     else:
+        print "enter an accepetable value"
+        pwm.ChangeDutyCycle(0) 
+        
 # If keyboard Interrupt (CTRL-C) is pressed
 except KeyboardInterrupt:
-    pass        # Go to next line
-pwm.stop()      # Stop the PWM
+    pass        
+pwm.stop()     
 GPIO.cleanup()  # Make all the output pins LOW
