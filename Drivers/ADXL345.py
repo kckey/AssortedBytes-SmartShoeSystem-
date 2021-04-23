7bab2be476583fddcb034d86b4047068d6c3f448
johnherring <60267462+johnherring@users.noreply.github.com>
2021-04-23 17:09:32 -0400

    Update ADXL345.py

--- Drivers/ADXL345.py
+++ Drivers/ADXL345.py
@@ -1,28 +1,71 @@
 import time
 import board
 import busio
+import array
+import math
 import adafruit_adxl34x
-import matplotlib.pyplot as plt
-import numpy as np
-import csv #imports csv package
 
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
-f = open("accelerometer_data.csv","w", newline="") #names and opens csv file that would be written to
-c = csv.writer(f)
-
-c.writerow(["X-Value","Y-Value","Z-Value","Time"]) #writes first row of csv file
-
-start_time = time.time() #function to start timing
+accel = []
+threshold = 3.0
+steps = 0
+height = 72
+stride = 0
+start = time.time()
 
 while True:
-    print("%f %f %f"%accelerometer.acceleration)
-    current_time = time.time() #assesing in time measurement
-    elapsed_time = current_time - start_time
-    c.writerow([accelerometer[1], accelerometer[2], accelerometer[3], elapsed_time]) #calls X,Y,& Z values from accelerometer and plots them into csv
-    time.sleep(0.001)
+    StrideTime = time.time() - start
+    print(StrideTime)
+    x, y, z = accelerometer.acceleration
+    mag = math.sqrt((x**2)+(y**2)+(z**2))
+    accel.append(mag)
+    mean = sum(accel) / len(accel)
+    magzero = mag - mean
+    #print(magzero)
+    #print(accelerometer.acceleration)
+    time.sleep(0.25)
 
+    if ( magzero > threshold ):
+        steps = steps + 1
+        print(steps)
+    else:
+        print( "No step detected" )
 
-plt.plot(accelerometer)
-plt.show()
+        if (StrideTime <= 2) & (steps < 2):
+            stride = height / 5
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (2 <= steps < 3):
+            stride = height / 4
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (3 <= steps < 4):
+            stride = height / 3
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (4 <= steps < 5):
+            stride = height / 2
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (5 <= steps < 6):
+            stride = height / 1.2
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (6 <= steps < 8):
+            stride = height
+            print(stride)
+        else:
+            print("nothing happened")
+        if (StrideTime <= 2) & (steps >= 8):
+            stride = height * 1.2
+            print(stride)
+        else:
+            print("nothing happened")
+            
